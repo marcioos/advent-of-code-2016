e976496f4cc29bd813157a2df11f83b83ba14cb1
Marcio de Oliveira da Silva <marcio.oliveira@klarna.com>
2016-12-05 13:09:10 +0100

    Day 3 part 2

--- src/03-squares-with-three-sides.js
+++ src/03-squares-with-three-sides.js
@@ -1,7 +1,7 @@
 const input = ['883 357 185',
                '572 189 424',
                '842 206 272',
-               '55 656 94',
+               '55 656 94', //0,1,2 9,10,11 18,19,20 27,28,29
                '612 375 90',
                '663 550 179',
                '183 487 470',
@@ -1901,17 +1901,39 @@ const input = ['883 357 185',
                '401 211 328',
                '725 312 215']
 
-const parsedInput = input.map((sides) =>
+const parsedInputForPart1 = input.map((sides) =>
   sides.split(/\s+/).map((side) =>
     parseInt(side)
   )
 )
 
-const result = parsedInput.map((sides) => {
+// 0, 3, 6
+// 1, 4, 7
+// 2, 5, 8
+
+const allSides = parsedInputForPart1.reduce((a, b) => a.concat(b))
+
+const parsedInputForPart2 = []
+
+var acc = 0
+var i = 0
+
+while (i <= allSides.length - 6) {
+  parsedInputForPart2.push([allSides[i], allSides[i + 3], allSides[i + 6]])
+  if (++acc === 3) {
+    i += 7
+    acc = 0
+  } else {
+    i++
+  }
+}
+
+const countValidTriangles = (input) => input.map((sides) => {
   if ((sides[0] + sides[1] <= sides[2]) || (sides[1] + sides[2] <= sides[0]) || (sides[2] + sides[0] <= sides[1])) {
     return 0
   }
   return 1
 }).reduce(((a, b) => a + b), 0)
 
-console.log(result)
+console.log(countValidTriangles(parsedInputForPart1))
+console.log(countValidTriangles(parsedInputForPart2))
